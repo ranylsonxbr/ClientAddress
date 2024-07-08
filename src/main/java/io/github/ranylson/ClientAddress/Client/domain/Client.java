ccec2975657ee99cb6ec94c517ed11ff845a4ca1
ranylsonxbr <ranylson1212@gmail.com>
2024-07-08 18:42:24 -0300

    implementa dominio Client

---
+++ src/main/java/io/github/ranylson/ClientAddress/Client/domain/Client.java
@@ -0,0 +1,28 @@
+package io.github.ranylson.ClientAddress.Client.domain;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    @Email
+    private String email;
+
+}
